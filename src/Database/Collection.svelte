e872f2bc5d334487744eca961a4c2d7d245d7096
THEGamingninja <thegamingninjamj@gmail.com>
2022-02-21 13:19:40 -0500

    Collection: Add sum default value

--- src/Database/Collection.svelte
+++ src/Database/Collection.svelte
@@ -82,7 +82,7 @@
 {#await getDocuments}
   <slot name="loading" />
 {:then current}
-  <slot documents={current?.documents ?? []} sum={current.sum} {actions} />
+  <slot documents={current?.documents ?? []} sum={current?.sum ?? 0} {actions} />
 {:catch error}
   <slot name="error" {error} />
 {/await}
